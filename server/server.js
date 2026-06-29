6b7ec24d97b629d77c58b486df63cfb40ade9120
Richa <richaaabinay03@gmail.com>
2026-06-29 18:42:27 +0530

    Day 2: MongoDB connected and Lead model created

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
-  res.send("CRM Server Running");
+  res.send("CRM API Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
